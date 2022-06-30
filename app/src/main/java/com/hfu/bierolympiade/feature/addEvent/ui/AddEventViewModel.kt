3b5a912bf70a3f21100072d3d3f02159a678dcfb
Jonathan <jonathan.rissler@gmx.de>
2022-06-30 14:36:41 +0200

    Auto stash before merge of "main" and "origin/main"

--- app/src/main/java/com/hfu/bierolympiade/feature/addEvent/ui/AddEventViewModel.kt
+++ app/src/main/java/com/hfu/bierolympiade/feature/addEvent/ui/AddEventViewModel.kt
@@ -107,9 +107,12 @@ class AddEventViewModel @Inject constructor(
     }
 
     private fun roundLowerEven(value: Double): Int = (kotlin.math.floor(value / 2) * 2).toInt()
+<<<<<<< Updated upstream
 
     suspend fun getEvent(eventId: String): Event?{
         return getEventById(EventId(eventId))
     }
 
+=======
+>>>>>>> Stashed changes
 }
